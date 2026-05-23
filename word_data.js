f255792ce35518a79039bcfb1c9d641e7c7276f2
JayceLee-Lab <jerrise.lee@gmail.com>
2026-05-23 09:49:05 +0900

    Add files via upload

---
+++ word_data.js
@@ -0,0 +1,32 @@
+const WORD_DATA = [
+    {
+        "hanzi": "苹果",
+        "pinyin": "píngguǒ",
+        "meaning": "사과",
+        "weight": 1
+    },
+    {
+        "hanzi": "学校",
+        "pinyin": "xuéxiào",
+        "meaning": "학교",
+        "weight": 1
+    },
+    {
+        "hanzi": "工作",
+        "pinyin": "gōngzuò",
+        "meaning": "일하다 / 직장",
+        "weight": 1
+    },
+    {
+        "hanzi": "医生",
+        "pinyin": "yīshēng",
+        "meaning": "의사",
+        "weight": 1
+    },
+    {
+        "hanzi": "飞机",
+        "pinyin": "fēijī",
+        "meaning": "비행기",
+        "weight": 1
+    }
+];
