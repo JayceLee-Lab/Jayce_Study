2091ddc5cd1dbada4834700101cab82afe83ff82
JayceLee-Lab <jerrise.lee@gmail.com>
2026-06-08 23:41:16 +0900

    Update word_data.js

--- word_data.js
+++ word_data.js
@@ -38,5 +38,85 @@ const WORD_DATA = [
         "a": "这是我家的地址",
         "aP": "Zhè shì wǒ jiā de dìzhǐ",
         "weight": 1
+    },
+        {
+        "hanzi": "生日礼物",
+        "pinyin": "shēngrì lǐwù",
+        "meaning": "생일 선물",
+        "a": "这是我爱人给我买的生日礼物",
+        "aP": "Zhè shì wǒ àiren gěi wǒ mǎi de shēngrì lǐwù",
+        "weight": 1
+    },
+    {
+        "hanzi": "穿",
+        "pinyin": "chuān",
+        "meaning": "입다.",
+        "a": "你穿这个颜色非常好看",
+        "aP": "nǐ chuān zhège yánsè fēicháng hǎokàn",
+        "weight": 1
+    },
+    {
+        "hanzi": "看起来",
+        "pinyin": "kànqǐlái",
+        "meaning": "그렇게 보이다.",
+        "a": "黑色看起来很漂亮",
+        "aP": "hēisè kànqǐlái hěn piàoliang",
+        "weight": 1
+    },
+    {
+        "hanzi": "试试",
+        "pinyin": "shìshì",
+        "meaning": "시험삼아 해보다. (입어보다.)",
+        "a": "可以试试吗?",
+        "aP": "Kěyǐ shìshì ma?",
+        "weight": 1
+    },
+    {
+        "hanzi": "喜欢的话",
+        "pinyin": "xǐhuān dehuà",
+        "meaning": "좋아한다면 (的话 : 만약에)",
+        "a": "你喜欢的话我给你买",
+        "aP": "nǐ xǐhuān dehuà wǒ gěi nǐ mǎi.",
+        "weight": 1
+    },
+    {
+        "hanzi": "网购",
+        "pinyin": "wǎnggòu",
+        "meaning": "온라인쇼핑",
+        "a": "我喜欢网购",
+        "aP": "wǒ xǐhuān wǎnggòu",
+        "weight": 1
+    },
+    {
+        "hanzi": "而且",
+        "pinyin": "érqiě",
+        "meaning": "그리고",
+        "a": "而且菜也好吃",
+        "aP": "érqiě cài yě hǎochī",
+        "weight": 1
+    },
+    {
+        "hanzi": "午饭时间",
+        "pinyin": "Wǔfàn shíjiān",
+        "meaning": "점심 시간",
+        "a": "午饭时间到了",
+        "aP": "Wǔfàn shíjiān dào le",
+        "weight": 1
+    },
+    {
+        "hanzi": "边吃边谈",
+        "pinyin": "biān chī biān tán",
+        "meaning": "먹으면서 얘기하다",
+        "a": "咱们边吃边谈吧",
+        "aP": "zánmen biān chī biān tán ba",
+        "weight": 1
+    },
+    {
+        "hanzi": "差",
+        "pinyin": "chà",
+        "meaning": "끔찍하다. 수준이 떨어지다.",
+        "a": "服务态度太差了",
+        "aP": "fúwù tàidù tài chà le",
+        "weight": 1
     }
 ];
