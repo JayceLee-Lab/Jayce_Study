4d0fad4881be6d72b44175cb3a267fa76d2d0129
JayceLee-Lab <jerrise.lee@gmail.com>
2026-06-08 23:13:55 +0900

    Update word_data.js

--- word_data.js
+++ word_data.js
@@ -1,32 +1,50 @@
 const WORD_DATA = [
     {
-        "hanzi": "苹果",
-        "pinyin": "píngguǒ",
-        "meaning": "사과",
+        "hanzi": "最近 流行",
+        "pinyin": "zuìjìn liúxíng",
+        "meaning": "최근 유행",
+        "a": "最近流行什么?",
+        "aP": "zuìjìn liúxíng shénme?",
         "weight": 1
     },
     {
-        "hanzi": "学校",
-        "pinyin": "xuéxiào",
-        "meaning": "학교",
+        "hanzi": "新出 的 款式",
+        "pinyin": "xīn chū de kuǎnshì",
+        "meaning": "새로 나온 디자인",
+        "a": "这是今年新出的款式，您试试吧",
+        "aP": "Zhè shì jīnnián xīn chū de kuǎnshì, nín shìshì ba.",
         "weight": 1
     },
     {
-        "hanzi": "工作",
-        "pinyin": "gōngzuò",
-        "meaning": "일하다 / 직장",
+        "hanzi": "不过",
+        "pinyin": "Búguò",
+        "meaning": "",
+        "a": "不过我不喜欢这个颜色, 这款车有黑色吗?",
+        "aP": "Búguò wǒ bù xǐhuān zhège yánsè, zhè kuǎn chē yǒu hēisè ma?",
         "weight": 1
     },
     {
-        "hanzi": "医生",
-        "pinyin": "yīshēng",
-        "meaning": "의사",
+        "hanzi": "不过",
+        "pinyin": "Búguò",
+        "meaning": "하지만",
+        "a": "不过我不喜欢这个颜色",
+        "aP": "Búguò wǒ bù xǐhuān zhège yánsè",
         "weight": 1
     },
     {
-        "hanzi": "飞机",
-        "pinyin": "fēijī",
-        "meaning": "비행기",
+        "hanzi": "家具",
+        "pinyin": "家具",
+        "meaning": "가구",
+        "a": "您买的家具",
+        "aP": "Nín mǎi de jiājù",
+        "weight": 1
+    },
+    {
+        "hanzi": "地址",
+        "pinyin": "dìzhǐ",
+        "meaning": "주소",
+        "a": "这是我家的地址",
+        "aP": "Zhè shì wǒ jiā de dìzhǐ",
         "weight": 1
     }
 ];
