7e2067122f22bfc1a7580f85965dd52694723b83
JayceLee-Lab <jerrise.lee@gmail.com>
2026-06-15 20:47:24 +0900

    Update word_data.js (3-2. ~61)

--- word_data.js
+++ word_data.js
@@ -160,11 +160,19 @@ const WORD_DATA = [
         "weight": 1
     },
     {
-        "hanzi": "资料",
-        "pinyin": "zīliào",
-        "meaning": "자료, 재료",
+        "hanzi": "写 资料",
+        "pinyin": "xiě zīliào",
+        "meaning": "자료를 정리하다.",
         "a": "你帮我写资料可以吗？",
         "aP": "Nǐ bāng wǒ xiě zīliào kěyǐ ma?",
+        "weight": 1
+    },
+    {
+        "hanzi": "寄",
+        "pinyin": "jì",
+        "meaning": "보내다",
+        "a": "您能寄给我新产品的资料吗？",
+        "aP": "Nín néng jì gěi wǒ xīn chǎnpǐn de zīliào ma?",
         "weight": 1
     },
     {
@@ -173,6 +181,110 @@ const WORD_DATA = [
         "meaning": "신제품",
         "a": "您能寄给我新产品的资料吗？",
         "aP": "Nín néng jì gěi wǒ xīn chǎnpǐn de zīliào ma?",
+        "weight": 1
+    },
+    {
+        "hanzi": "考试结束",
+        "pinyin": "Kǎoshì jiéshù",
+        "meaning": "시험 종료",
+        "a": "考试结束了，周六一起看电影吧",
+        "aP": "Kǎoshì jiéshù le, zhōuliù yìqǐ kàn diànyǐng ba.",
+        "weight": 1
+    },
+    {
+        "hanzi": "打工",
+        "pinyin": "dǎgōng",
+        "meaning": "아르바이트",
+        "a": "周六我要去打工。",
+        "aP": "zhōuliù wǒ yào qù dǎgōng.",
+        "weight": 1
+    },
+    {
+        "hanzi": "找",
+        "pinyin": "zhǎo",
+        "meaning": "찾다.",
+        "a": "你找别人吧。",
+        "aP": "Nǐ zhǎo biérén ba.",
+        "weight": 1
+    },
+    {
+        "hanzi": "方便",
+        "pinyin": "fāngbiàn",
+        "meaning": "편리하다.",
+        "a": "吃汉堡又好吃又方便，而且省时。",
+        "aP": "chī hànbǎo yòu hǎochī yòu fāngbiàn, érqiě shěngshí.",
+        "weight": 1
+    },
+    {
+        "hanzi": "而且",
+        "pinyin": "érqiě",
+        "meaning": "또한",
+        "a": "吃汉堡又好吃又方便，而且省时。",
+        "aP": "chī hànbǎo yòu hǎochī yòu fāngbiàn, érqiě shěngshí.",
+        "weight": 1
+    },
+    {
+        "hanzi": "省时",
+        "pinyin": "shěngshí",
+        "meaning": "시간을 절약하다.",
+        "a": "吃汉堡又好吃又方便，而且省时。",
+        "aP": "chī hànbǎo yòu hǎochī yòu fāngbiàn, érqiě shěngshí.",
+        "weight": 1
+    },
+    {
+        "hanzi": "关上窗户",
+        "pinyin": "guānshang chuānghu",
+        "meaning": "창문을 닫다.",
+        "a": "你能帮我关上窗户吗？",
+        "aP": "nǐ néng bāng wǒ guānshang chuānghu ma?",
+        "weight": 1
+    },
+    {
+        "hanzi": "健身房",
+        "pinyin": "jiànshēnfán",
+        "meaning": "헬스장",
+        "a": "我下班以后经常去健身房运动",
+        "aP": "Wǒ xiàbān yǐhòu jīngcháng qù jiànshēnfáng yùndòng",
+        "weight": 1
+    },
+    {
+        "hanzi": "新闻",
+        "pinyin": "xīnwén",
+        "meaning": "뉴스",
+        "a": "你昨天看新闻了吗？",
+        "aP": "Nǐ zuótiān kàn xīnwén le ma?",
+        "weight": 1
+    },
+    {
+        "hanzi": "所以",
+        "pinyin": "Suǒyǐ",
+        "meaning": "그래서",
+        "a": "所以我的手机里有很多照片。",
+        "aP": "Suǒyǐ wǒ de shǒujī lǐ yǒu hěn duō zhàopiàn.",
+        "weight": 1
+    },
+    {
+        "hanzi": "看起来",
+        "pinyin": "kàn qǐlái",
+        "meaning": "보기에",
+        "a": "你看起来很累。",
+        "aP": "Nǐ kàn qǐlái hěn lèi.",
+        "weight": 1
+    },
+    {
+        "hanzi": "早睡早起",
+        "pinyin": "Zǎoshuì zǎoqǐ",
+        "meaning": "일찍 자고 일찍 일어나다.",
+        "a": "早睡早起对身体好。",
+        "aP": "Zǎoshuì zǎoqǐ duì shēntǐ hǎo.",
+        "weight": 1
+    },
+    {
+        "hanzi": "自由",
+        "pinyin": "zìyóu",
+        "meaning": "자유롭다",
+        "a": "我觉得一个人住比较自由。",
+        "aP": "Wǒ juéde yí ge rén zhù bǐjiào zìyóu.",
         "weight": 1
     }
 ];
